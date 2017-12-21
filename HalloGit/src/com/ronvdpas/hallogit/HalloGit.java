78f560621db373470529b291222e89834f0908ef
rovdpas <31190318+rovdpas@users.noreply.github.com>
2017-12-21 19:52:09 +0100

    Commentaar regel toegevoegd

--- HalloGit/src/com/ronvdpas/hallogit/HalloGit.java
+++ HalloGit/src/com/ronvdpas/hallogit/HalloGit.java
@@ -3,6 +3,8 @@ package com.ronvdpas.hallogit;
 public class HalloGit {
 
 	public static void main(String[] args) {
+		
+		// Een commentaar regel toegevoegd
 		System.out.println("Hallo GIT!");
 
 	}
