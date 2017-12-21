08dec98aedebfa5f0fa8bf39aef19c313c3fcd57
rovdpas <31190318+rovdpas@users.noreply.github.com>
2017-12-21 20:10:15 +0100

    Eerste ontwikkelaar wijzigt alleen regel 2

--- HalloGit/src/com/ronvdpas/hallogit/TestGITBranche.java
+++ HalloGit/src/com/ronvdpas/hallogit/TestGITBranche.java
@@ -6,7 +6,7 @@ public class TestGITBranche {
 
 		System.out.println("Eerste regel van de file");
 		
-		System.out.println("Tweede regel van de file");
+		System.out.println("Tweede regel van de file die ik nu in de eerste file wijzig");
 
 	}
 
