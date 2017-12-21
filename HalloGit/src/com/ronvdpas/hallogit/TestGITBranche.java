e4ff47f6b14d951e7abd87cba8de7c0f25fadfb6
rovdpas <31190318+rovdpas@users.noreply.github.com>
2017-12-21 20:12:45 +0100

    Eerste en tweede regel is gewijzigd

--- HalloGit/src/com/ronvdpas/hallogit/TestGITBranche.java
+++ HalloGit/src/com/ronvdpas/hallogit/TestGITBranche.java
@@ -4,9 +4,9 @@ public class TestGITBranche {
 
 	public static void main(String[] args) {
 
-		System.out.println("Eerste regel van de file");
+		System.out.println("Eerste regel van de file en wijzigingen op een unieke regel");
 		
-		System.out.println("Tweede regel van de file");
+		System.out.println("Tweede regel van de file met wijziginen ook voor developer 1");
 
 	}
 
